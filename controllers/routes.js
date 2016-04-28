ba455a42983af83f070ed4a646910620e3b240a1
Jose Mora <jdevmora@gmail.com>
2016-04-28 01:08:53 -0600

    - Borrado de archivos de Node.js
    Se dejo solamente el screen de mantenimiento como fijo.

--- controllers/routes.js
+++
@@ -1,6 +0,0 @@
-var controllers = require('./controllers');
-
-module.exports = function (router) {
-    router.get('/', controllers.index);
-    router.get('/setLocale/:locale', controllers.setLocale);
-};
